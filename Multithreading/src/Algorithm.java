d9ba9a223629b32ebbd334bd7ec9dd9d29ad7ce6
Anton Prokopev <a.prokopev@innopolis.ru>
2018-01-23 20:48:41 +0300

    Clean master.

--- Multithreading/src/Algorithm.java
+++
@@ -1,5 +0,0 @@
-public interface Algorithm {
-    void processNextSymbol(char c) throws DuplicateWordException, UnexpectedSymbolException;
-    void flush() throws DuplicateWordException;
-    void resetAlgorithm();
-}
--- Multithreading/src/Fetcher.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface Fetcher {
-    List<String> fetchAll() throws IOException;
-}
