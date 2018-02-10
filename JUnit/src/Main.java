be992cd33e62c758939acbedfc7915cd7e30d9b4
prokop7 <a.prokopev@innopolis.ru>
2018-02-10 20:13:17 +0300

    End of lecture

--- JUnit/src/Main.java
+++ JUnit/src/Main.java
@@ -1,17 +1,35 @@
-import java.time.Clock;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
 public class Main {
+    private final static Set<String> set = new HashSet<>();
+
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("123 ");
-        list.add(" 123");
-        list.add(" 123 ");
-        String reduce = list.stream().reduce("", (v, a) -> v + a);
-        System.out.println(reduce);
+        boolean unique = Arrays
+                .stream(args)
+                .parallel()
+                .map(Main::openFile)
+                .filter(Objects::nonNull)
+                .flatMap(BufferedReader::lines)
+                .flatMap(s -> Arrays.stream(s.split(" ")))
+                .anyMatch(Main::checkWord);
+        System.out.println(unique);
+    }
+
+    private static BufferedReader openFile(String s) {
+        try {
+            return new BufferedReader(new FileReader(s));
+        } catch (FileNotFoundException e) {
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+
+    private static boolean checkWord(String s) {
+        synchronized (set) {
+            return !set.add(s);
+        }
     }
 }
