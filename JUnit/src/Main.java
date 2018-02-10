05f4ad398f5f0ef4d8e18b61b91025654763b2c0
prokop7 <a.prokopev@innopolis.ru>
2018-02-10 18:49:36 +0300

    Streams

--- JUnit/src/Main.java
+++ JUnit/src/Main.java
@@ -1,5 +1,17 @@
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<>();
+        list.add("123 ");
+        list.add(" 123");
+        list.add(" 123 ");
+        String reduce = list.stream().reduce("", (v, a) -> v + a);
+        System.out.println(reduce);
     }
 }
