28cf28d52ac0d3aafd15861cd6a7af080a4296ed
prokop7 <a.prokopev@innopolis.ru>
2018-02-28 23:03:47 +0300

    Wishlist-servlet

---
+++ wishlist/src/main/java/controllers/HomeServlet.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "myHome", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("userName", "123123");
+
+        getServletContext()
+                .getRequestDispatcher("/home.jsp")
+                .forward(req, resp);
+    }
+}
